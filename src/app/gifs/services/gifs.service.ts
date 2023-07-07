e976594ed0374fb772a99f51161bf55ceffaca12
Pablo <pabloag43@educastur.es>
2023-07-07 13:29:23 +0200

    Añadimos la request a la api con HTTP

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
 
+  public gifsList: Gif[] = [];
+
   private _tagsHistory: string[] = [];
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs'
+  private apiKey: string = 'f6HlH9VFS7Gnq5UNsDHHbr7wEM0ZWJez';
 
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
 
   get tagsHistory() {
@@ -27,11 +34,22 @@ export class GifsService {
     this._tagsHistory = this._tagsHistory.splice(0, 10)
   }
 
-  public searchTag(tag: string): void {
+
+  searchTag(tag: string): void {
+    if (tag.length === 0) return;
     this.organizeHistory(tag);
 
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag)
 
 
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe(resp => {
+        this.gifsList = resp.data
+        console.log({ gifs: this.gifsList })
+      })
 
   }
 
